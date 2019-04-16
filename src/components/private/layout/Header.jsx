e06db8946df7c11718bc6b6250cb8c6e72c00f3c
Takafumi Iju <ijufumi@gmail.com>
2019-04-16 23:58:35 +0900

    Add header css.

--- src/components/private/layout/Header.jsx
+++ src/components/private/layout/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navigation from "react-toolbox/lib/navigation";
 import {Button} from "react-toolbox/lib/button";
 
+import styles from './theme.css'
+
 class Header extends React.Component {
     render() {
         return(
-            <Navigation type='horizontal'>
+            <Navigation type='horizontal' className={styles.header}>
                 <Button label='Home' raised/>
                 <Button label='Home' raised/>
                 <Button label='Home' raised/>
