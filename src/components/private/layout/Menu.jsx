defc4145997f23a60a570d7d22d768fea0d78a60
Takafumi Iju <ijufumi@gmail.com>
2019-04-19 23:46:11 +0900

    [WIP] Change icons.

--- src/components/private/layout/Menu.jsx
+++ src/components/private/layout/Menu.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import {Accordion} from 'grommet/components/Accordion'
 import {AccordionPanel} from 'grommet/components/AccordionPanel'
 import {Box} from "grommet/components/Box";
-//import {Add} from 'grommet-icons'
+import {Add} from 'grommet-icons'
+import extendDefaultTheme from 'grommet/default-props'
+
+
+const accordionTheme = {
+    accordion: {
+        icons: {
+            collapse: <Add/>,
+            expand: <Add/>,
+        },
+        border : {"side": "bottom", "color": "border"},
+    }
+} | extendDefaultTheme;
 
 class SideMenu extends React.Component {
     render() {
         return (
-            <Accordion multiple="true">
-                <AccordionPanel label="nav1">
+            <Accordion multiple={true}>
+                <AccordionPanel label="nav1" theme={accordionTheme}>
                     <Box>
                         menu1
                     </Box>
