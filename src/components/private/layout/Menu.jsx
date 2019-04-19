7823dbb60827b55df263aa1c0d315cea29d69f3a
Takafumi Iju <ijufumi@gmail.com>
2019-04-19 20:00:41 +0900

    Modify private top page.

--- src/components/private/layout/Menu.jsx
+++ src/components/private/layout/Menu.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {Accordion} from 'grommet/components/Accordion'
+import {AccordionPanel} from 'grommet/components/AccordionPanel'
+import {Box} from "grommet/components/Box";
+//import {Add} from 'grommet-icons'
 
 class SideMenu extends React.Component {
     render() {
         return (
-            <div></div>
+            <Accordion multiple="true">
+                <AccordionPanel label="nav1">
+                    <Box>
+                        menu1
+                    </Box>
+                    <Box>
+                        menu2
+                    </Box>
+                    <Box>
+                        menu3
+                    </Box>
+                </AccordionPanel>
+                <AccordionPanel label="nav2">
+                    <Box>
+                        menu1
+                    </Box>
+                    <Box>
+                        menu2
+                    </Box>
+                    <Box>
+                        menu3
+                    </Box>
+                </AccordionPanel>
+            </Accordion>
         );
     }
 }
--- src/components/private/top/Top.jsx
+++ src/components/private/top/Top.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
+import {Box} from 'grommet/components/Box'
+import {Heading} from 'grommet/components/Heading'
+
+import {Grid} from 'grommet/components/Grid'
+
+import SideMenu from '../layout/Menu'
 
 class Top extends React.Component {
     render() {
         return(
-            <div></div>
+            <div>
+                <Helmet>
+                    <title>OpenReports - Home</title>
+                </Helmet>
+                <Grid rows={['large']}
+                      columns={['small', 'xlarge']}
+                      gap="small"
+                      areas={[
+                          { name: 'nav', start: [0, 0], end: [0, 0] },
+                          { name: 'main', start: [1, 0], end: [1, 0] }
+                      ]}>
+
+                    <Box gridArea="main" width="medium">
+                        <Heading>Home</Heading>
+                    </Box>
+                    <Box gridArea="nav" width="medium">
+                        <SideMenu/>
+                    </Box>
+
+                </Grid>
+
+            </div>
         );
     }
 }
