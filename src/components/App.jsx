5cd48446e3c54be2cea7cf9506847993be24ed10
Takafumi Iju <ijufumi@gmail.com>
2019-05-08 22:56:59 +0900

    Add Switch tag.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Top from './public/top/Top'
 import PrivateTop from './private/top/Top'
@@ -14,15 +14,17 @@ import ReportParamTop from './private/settings/report-param/index'
 const App = () => (
     <BrowserRouter>
         <div>
-            <Route exact path='/' component={Top}/>
-            <Route exact path='/private/' component={PrivateTop}/>
-            <Route exact path='/private/settings/member' component={MemberTop}/>
-            <Route exact path='/private/settings/member/register' component={MemberRegister}/>
-            <Route exact path='/private/settings/group' component={GroupTop}/>
-            <Route exact path='/private/settings/report' component={ReportTop}/>
-            <Route exact path='/private/settings/report-group' component={ReportGroupTop}/>
-            <Route exact path='/private/settings/report-template' component={ReportTemplateTop}/>
-            <Route exact path='/private/settings/report-param' component={ReportParamTop}/>
+            <Switch>
+                <Route exact path='/' component={Top}/>
+                <Route exact path='/private/' component={PrivateTop}/>
+                <Route exact path='/private/settings/member' component={MemberTop}/>
+                <Route exact path='/private/settings/member/register' component={MemberRegister}/>
+                <Route exact path='/private/settings/group' component={GroupTop}/>
+                <Route exact path='/private/settings/report' component={ReportTop}/>
+                <Route exact path='/private/settings/report-group' component={ReportGroupTop}/>
+                <Route exact path='/private/settings/report-template' component={ReportTemplateTop}/>
+                <Route exact path='/private/settings/report-param' component={ReportParamTop}/>
+            </Switch>
         </div>
     </BrowserRouter>
 );
