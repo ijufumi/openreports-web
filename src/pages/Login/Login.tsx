eaf0e61b54f74801184b27bf3700fc4c3495dc12
Takafumi Iju <ijufumi@gmail.com>
2022-05-15 08:56:38 +0900

    format

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,21 @@
-import React, { FC, useState } from 'react';
-import { Input, InputGroup, InputRightElement, Button, VStack, InputLeftElement, Icon, Box, Flex, Text } from '@chakra-ui/react'
-import { MdOutlineEmail } from 'react-icons/md'
-import { CgPassword } from 'react-icons/cg'
-import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import React, { FC, useState } from "react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  VStack,
+  InputLeftElement,
+  Icon,
+  Box,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import { MdOutlineEmail } from "react-icons/md";
+import { CgPassword } from "react-icons/cg";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
-interface Props {
-
-}
+interface Props {}
 
 const Login: FC<Props> = () => {
   const [email, setEmail] = useState<string>("");
@@ -14,15 +23,28 @@ const Login: FC<Props> = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   return (
-    <Flex minWidth='100%' alignItems='center' justifyContent="center" minHeight='100%'>
-      <Box w='550px' bg={"white"} borderRadius="10px">
+    <Flex
+      minWidth="100%"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100%"
+    >
+      <Box w="550px" bg={"white"} borderRadius="10px">
         <VStack margin={"10px"} spacing={"25px"}>
-          <Text fontSize='3xl'>Login</Text>
+          <Text fontSize="3xl">Login</Text>
           <VStack width={"90%"} spacing={"15px"}>
             <InputGroup>
               <InputLeftElement
-                pointerEvents='none'
-                children={<Icon as={MdOutlineEmail} color='gray.500' w={10} h={10} style={{ padding: "0 5px 0 5px" }} />}
+                pointerEvents="none"
+                children={
+                  <Icon
+                    as={MdOutlineEmail}
+                    color="gray.500"
+                    w={10}
+                    h={10}
+                    style={{ padding: "0 5px 0 5px" }}
+                  />
+                }
               />
               <Input
                 id="email"
@@ -33,18 +55,32 @@ const Login: FC<Props> = () => {
             </InputGroup>
             <InputGroup>
               <InputLeftElement
-                pointerEvents='none'
-                children={<Icon as={CgPassword} color='gray.500' w={10} h={10} style={{ padding: "0 5px 0 5px" }} />}
+                pointerEvents="none"
+                children={
+                  <Icon
+                    as={CgPassword}
+                    color="gray.500"
+                    w={10}
+                    h={10}
+                    style={{ padding: "0 5px 0 5px" }}
+                  />
+                }
               />
               <Input
                 id="password"
                 value={password}
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 placeholder={"Enter your password"}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <InputRightElement width='4.5rem' cursor="pointer">
-                <Icon as={showPassword ? AiOutlineEye : AiOutlineEyeInvisible } color='gray.500' w={8} h={8} onClick={() => setShowPassword(!showPassword)} />
+              <InputRightElement width="4.5rem" cursor="pointer">
+                <Icon
+                  as={showPassword ? AiOutlineEye : AiOutlineEyeInvisible}
+                  color="gray.500"
+                  w={8}
+                  h={8}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
               </InputRightElement>
             </InputGroup>
           </VStack>
