ae176fc63821cde95efdd504b1b93d03e071733b
Takafumi Iju <ijufumi@gmail.com>
2022-05-17 06:48:43 +0900

    Modify layout.

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from "react";
-import { Stack } from "@chakra-ui/react";
+import { HStack, Box } from "@chakra-ui/react";
 
 interface Props {
   children: any;
 }
 
 const Layout: FC<Props> = ({ children }) => {
-  return <Stack>{children}</Stack>;
+  return (
+    <HStack spacing={"5px"}>
+      <Box w={"30px"}></Box>
+      <Box flexGrow={1}>{children}</Box>
+    </HStack>
+  );
 };
 
 export default Layout;
