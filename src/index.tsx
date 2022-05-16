75c1dc5bebc3d25a75643fee467f9561f4365278
Takafumi Iju <ijufumi@gmail.com>
2022-05-17 06:38:56 +0900

    Modify to use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./app";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container as Element);
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
