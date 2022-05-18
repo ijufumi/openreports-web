044771db0775efd63c5f56ad6e5699629d41cd45
Takafumi Iju <ijufumi@gmail.com>
2022-05-19 06:52:21 +0900

    Modify layout

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react";
 import {
   VStack,
+  HStack,
   Box,
   Menu,
   MenuList,
   MenuItem,
   MenuButton,
+  Icon,
+  IconButton,
 } from "@chakra-ui/react";
+import { FaRegUserCircle } from "react-icons/fa";
 
 interface Props {
   children: React.ReactNode;
@@ -16,23 +20,109 @@ const Layout: FC<Props> = ({ children }) => {
   return (
     <VStack spacing={"5px"} h={"100%"}>
       <Box
-        h={"50px"}
+        h={"70px"}
         w={"100%"}
         bg={"white"}
         display={"flex"}
         alignItems={"center"}
+        justifyContent={"space-between"}
         padding={"0 10px 0 10px"}
       >
-        <Menu>
-          <MenuButton>Test</MenuButton>
-          <MenuList>
-            <MenuItem>Item 1</MenuItem>
-            <MenuItem>Item 2</MenuItem>
-            <MenuItem>Item 3</MenuItem>
-          </MenuList>
-        </Menu>
+        <HStack spacing={"10px"}>
+          <Menu>
+            <MenuButton
+              sx={{
+                bg: "blue.50",
+                borderColor: "blue.200",
+                borderWidth: "1px",
+                borderRadius: "5px",
+                h: "40px",
+                w: "100px",
+              }}
+            >
+              Menu 1
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+              <MenuItem>Item 3</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              sx={{
+                bg: "blue.50",
+                borderColor: "blue.200",
+                borderWidth: "1px",
+                borderRadius: "5px",
+                h: "40px",
+                w: "100px",
+              }}
+            >
+              Menu 2
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+              <MenuItem>Item 3</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              sx={{
+                bg: "blue.50",
+                borderColor: "blue.200",
+                borderWidth: "1px",
+                borderRadius: "5px",
+                h: "40px",
+                w: "100px",
+              }}
+            >
+              Menu 3
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+              <MenuItem>Item 3</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              sx={{
+                bg: "blue.50",
+                borderColor: "blue.200",
+                borderWidth: "1px",
+                borderRadius: "5px",
+                h: "40px",
+                w: "100px",
+              }}
+            >
+              Menu 4
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+              <MenuItem>Item 3</MenuItem>
+            </MenuList>
+          </Menu>
+        </HStack>
+        <HStack>
+          <IconButton
+            aria-label="profile"
+            colorScheme="whiteAlpha"
+            icon={
+              <Icon
+                as={FaRegUserCircle}
+                color="gray.600"
+                w={10}
+                h={10}
+                style={{ padding: "0 5px 0 5px" }}
+              />
+            }
+          />
+        </HStack>
       </Box>
-      <Box w={"100%"} h={"100%"}>
+      <Box w={"100%"} h={"100%"} padding={"10px"}>
         {children}
       </Box>
     </VStack>
