9bae6a0fea7c2f8e253620da079f7c3caddf3014
Takafumi Iju <ijufumi@gmail.com>
2022-05-21 08:57:33 +0900

    Modify authorized

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -15,19 +15,33 @@ const Authorized: FC<AuthorizedRouteProps> = ({ children }) => {
   return <Layout>{children}</Layout>;
 };
 
+interface AuthorizedPath {
+  path: string;
+  element: ReactNode;
+}
+
 interface Props {}
 
 const App: FC<Props> = () => {
-  const renderAuthorized = (path: string, element: any) => {
+  const renderAuthorized = (path: string, element: ReactNode) => {
     return <Route path={path} element={<Authorized>{element}</Authorized>} />;
   };
+
+  const authorizedPaths: AuthorizedPath[] = [
+    {
+      path: "/top",
+      element: <Top />,
+    },
+  ];
 
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          {renderAuthorized("/top", <Top />)}
+          {authorizedPaths.map((e: AuthorizedPath) => {
+            return renderAuthorized(e.path, e.element);
+          })}
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
