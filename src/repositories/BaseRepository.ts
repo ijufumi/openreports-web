d08d60c07325c0c5a4eae00e6d3a811abcfd69af
Takafumi Iju <ijufumi@gmail.com>
2022-08-08 08:12:41 +0900

    Fixed bug

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -1,36 +1,49 @@
 enum Methods {
-    Get = "GET",
-    Put = "PUT",
-    Post = "POST",
-    Delete = "DELETE"
+  Get = "GET",
+  Put = "PUT",
+  Post = "POST",
+  Delete = "DELETE",
 }
 abstract class BaseRepository {
-    private readonly apiEndpoint: string;
+  private readonly apiEndpoint: string;
 
-    constructor(apiEndpoint: string) {
-        this.apiEndpoint = apiEndpoint;
-    }
+  constructor(apiEndpoint: string) {
+    this.apiEndpoint = apiEndpoint;
+  }
 
-    get = async (args: { path: string, headers?: object }) => {
-        return await this._request(Methods.Get, this.apiEndpoint, args.path, undefined, args.headers);
-    }
-    post = async (args: { path: string, headers?: object, body?: object}) => {
-        return await this._request(Methods.Post, this.apiEndpoint, args.path, args.body, args.headers);
-    }
+  get = async (args: { path: string; headers?: object }) => {
+    return await this._request(
+      Methods.Get,
+      this.apiEndpoint,
+      args.path,
+      undefined,
+      args.headers
+    );
+  };
+  post = async (args: { path: string; headers?: object; body?: object }) => {
+    return await this._request(
+      Methods.Post,
+      this.apiEndpoint,
+      args.path,
+      args.body,
+      args.headers
+    );
+  };
 
-    _request = async (method: Methods, apiEndpoint: string, path: string,
-        body?: object, header?: object) => {
-
-        return fetch(
-            apiEndpoint,
-            {
-                method: method.toString(),
-                body: body ? JSON.stringify(body) : "",
-            }
-        )
-            .then((res) => res.json())
-            .catch(e => console.log(e));
-    }
+  _request = async (
+    method: Methods,
+    apiEndpoint: string,
+    path: string,
+    body?: object,
+    header?: object
+  ) => {
+    return fetch(apiEndpoint, {
+      method: method.toString(),
+      body: body ? JSON.stringify(body) : undefined,
+    })
+      .then((res) => res.json())
+      .catch((e) => console.log(e));
+  };
 }
 
 export default BaseRepository;
