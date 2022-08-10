716edc440527292d6b10d30ef960ff6275719f98
Takafumi Iju <ijufumi@gmail.com>
2022-08-11 08:31:18 +0900

    Fixed path

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -37,7 +37,7 @@ abstract class BaseRepository {
     body?: object,
     header?: object
   ) => {
-    return fetch(`${apiEndpoint}/${path}`, {
+    return fetch(`${apiEndpoint}${path}`, {
       method: method.toString(),
       body: body ? JSON.stringify(body) : undefined,
     })
