33c8db3df17cdca4207d90c848325f7bbeb8514a
Takafumi Iju <ijufumi@gmail.com>
2022-10-17 10:41:48 +0900

    [WIP] report detail

--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
+import { useParams } from "react-router";
+import { HStack } from "@chakra-ui/react";
 
 interface Props {}
 
 const Report: FC<Props> = () => {
-  return null;
+  const params = useParams();
+  const id = params.id;
+
+  console.log(id);
+  return <HStack></HStack>;
 };
 
 export default Report;
