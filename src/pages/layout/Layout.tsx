3b97699f976252cf41408abd0db73cfb02409f77
Takafumi Iju <ijufumi@gmail.com>
2022-10-18 08:02:32 +0900

    Delete unnecessary imports

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useState, useEffect } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react";
 import {
