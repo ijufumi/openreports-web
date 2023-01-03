d8237ba790d61b344582bbc7d49532d3cf79526d
Takafumi Iju <ijufumi@gmail.com>
2023-01-03 11:49:43 +0900

    [WIP] add data source

---
+++ src/vos/DataSource.ts
@@ -0,0 +1,42 @@
+import DateUtils from "../components/utils/DateUtils";
+
+export interface Props {
+  id: string;
+  name: string;
+  url: string;
+  username: string;
+  password: string;
+  driverTypeId: string;
+  createdAt: number;
+  updatedAt: number;
+}
+
+export default class DataSourceVo {
+  readonly id: string;
+  readonly name: string;
+  readonly url: string;
+  readonly username: string;
+  readonly password: string;
+  readonly driverTypeId: string;
+  readonly createdAt: number;
+  readonly updatedAt: number;
+
+  constructor(prop: Props) {
+    this.id = prop.id;
+    this.name = prop.name;
+    this.url = prop.url;
+    this.username = prop.username;
+    this.password = prop.password;
+    this.driverTypeId = prop.driverTypeId;
+    this.createdAt = prop.createdAt;
+    this.updatedAt = prop.updatedAt;
+  }
+
+  get formattedCreatedAt() {
+    return DateUtils.format(this.createdAt);
+  }
+
+  get formattedUpdatedAt() {
+    return DateUtils.format(this.updatedAt);
+  }
+}
