2f6b348d96ea6c98c761ef278860751b7fca0fa4
Takafumi Iju <ijufumi@gmail.com>
2024-04-12 15:46:04 +0900

    feat: add new configuration file of eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import parser from "@typescript-eslint/parser";
+import eslintPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
@@ -7,10 +8,18 @@ export default [
     ignores: ["*.config.js", "**/*.config.js"],
     rules: {
       "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/strict-boolean-expressions": "warn",
+      "@typescript-eslint/no-empty-interface": 0
+    },
+    plugins:{
+      "@typescript-eslint": eslintPlugin
     },
     languageOptions: {
         globals: globals.browser,
         parser: parser,
+        parserOptions: {
+          project: "./tsconfig.json",
+        }
       },
     },
 ];
