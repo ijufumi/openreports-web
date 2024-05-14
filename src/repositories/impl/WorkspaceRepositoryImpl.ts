15929871ef38b077ae7cd15246d4fe3602c34069
Takafumi Iju <ijufumi@gmail.com>
2024-05-15 07:04:10 +0900

    feat: Add registration workspace

--- src/repositories/impl/WorkspaceRepositoryImpl.ts
+++ src/repositories/impl/WorkspaceRepositoryImpl.ts
@@ -1,22 +1,28 @@
-import BaseRepository from "./BaseRepository";
-import WorkspaceRepository from "../WorkspaceRepository";
-import WorkspaceVo from "../../vos/WorkspaceVo";
-import WorkspacesVo from "../../vos/WorkspacesVo";
+import BaseRepository from "./BaseRepository"
+import WorkspaceRepository from "../WorkspaceRepository"
+import WorkspaceVo from "../../vos/WorkspaceVo"
 
 export default class WorkspaceRepositoryImpl
   extends BaseRepository
   implements WorkspaceRepository
 {
   getById = async (args: { id: string }) => {
-    const path = `/${args.id}`;
-    const result = await this._get({ path });
-    return new WorkspaceVo(result);
-  };
+    const path = `/${args.id}`
+    const result = await this._get({ path })
+    return new WorkspaceVo(result)
+  }
+
+  register = async (args: { name: string }) => {
+    const path = `/`
+    const body = { name: args.name }
+    const result = await this._post({ path, body })
+    return new WorkspaceVo(result)
+  }
 
   update = async (args: { id: string; name: string }) => {
-    const path = `/${args.id}`;
-    const body = { name: args.name };
-    const result = await this._put({ path, body });
-    return new WorkspaceVo(result);
-  };
+    const path = `/${args.id}`
+    const body = { name: args.name }
+    const result = await this._put({ path, body })
+    return new WorkspaceVo(result)
+  }
 }
